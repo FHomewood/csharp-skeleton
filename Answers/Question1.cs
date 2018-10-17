2b7689146f0b6dbdee2fe8d9852fb7a43bb09808
fhomewood <fhomewood@gmail.com>
2018-10-17 11:24:20 +0100

    first attempt at brute force Q1 method to be refined later

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -10,8 +10,57 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] portfolios)
         {
+            //just a quick brute force method
+            int bestVal = 0;
+            int[] bestCombo = new int[15];
+            int[] A = new int[15];
+            int[] B = new int[15];
+            for (int i = 0; i < portfolios.Length-1;i++)
+            {
+                for (int j = portfolios.Length-1; j > 0; j++)
+                {
+                    int[] p1 = ToPortfolio(portfolios[i]);
+                    int[] p2 = ToPortfolio(portfolios[j]);
+                    int[] Combo = Combine(p1, p2);
+                    int Value = ToValue(Combo);
+                        if (Value > bestVal)
+                        {
+                            bestVal = Value;
+                            B = p2;
+                            A = p1;
+                        }
+                }
+            }
             //TODO: Please work out the solution;
-            return -1;
+            return bestVal;
+
+            int ToValue(int[] a)
+            {
+                double output = 0;
+                for (int k = 0; k < a.Length; k++) output += a[k] * Math.Pow(2, k);
+                return (int)output;
+            }
+            int[] ToPortfolio(int a)
+            {
+                int[] output = new int[15];
+                for (int k = output.Length - 1; k >= 0; k--)
+                {
+                    int pow = (int)Math.Pow(2, k);
+                    output[k] = (a % pow == 1) ? 1 : 0;
+                    a -= pow * output[15];
+
+                }
+                return output;
+            }
+            int[] Combine(int[] a, int[] b)
+            {
+                int[] c = new int[15];
+                for (int k = 0; k < 15; k++)
+                {
+                    c[k] = a[k] ^ b[k];
+                }
+                return c;
+            }
         }
     }
 }
