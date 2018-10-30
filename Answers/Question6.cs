559ebdbeafcb4bd931fafad805202ebda84b3a9a
fhomewood <fhomewood@gmail.com>
2018-10-30 18:45:05 +0000

    Added the two step solution but I doubt I'll really get any closer than that

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -1,10 +1,17 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+using System.Linq;
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question6
     {
         public static int Answer(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
         {
             int shortestPath = connectionTimeMatrix[targetServer,0];
+            for (int i = 0; i < numOfServers; i++)
+            {
+                int n = connectionTimeMatrix[i, 0] + connectionTimeMatrix[targetServer, i];
+                if (n < shortestPath) shortestPath = n;
+            }
             return shortestPath;
         }
     }
