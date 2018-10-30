9624a7f7e82bcd42827912f2e179ebdfc2f50529
fhomewood <fhomewood@gmail.com>
2018-10-30 19:23:06 +0000

    One more brute force attempt added to the list, Q4 may or may not be functional

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -4,8 +4,22 @@
     {
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            int lowest = int.MaxValue;
+            for (int i = 0; i < machineToBeFixed.GetLength(0); i++)
+            {
+                int total = 0;
+                int noX = 0;
+                for (int j = 0; j < machineToBeFixed.GetLength(1); j++)
+                {
+                    bool ifX = machineToBeFixed[i, j] == "X";
+                    total +=  ifX? int.Parse(machineToBeFixed[i,j]) : 0;
+                    noX += ifX ? 0 : 1;
+                }
+                lowest = total < lowest && machineToBeFixed.GetLength(1) - noX == 0 ?
+                    total:
+                    lowest;
+            }
+            return lowest;
         }
     }
 }
